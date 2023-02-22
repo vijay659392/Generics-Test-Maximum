579c38b562df03738967a26be2752b5cd7722c19
vijay <vijaydurkikar1@gmail.com>
2023-02-22 12:33:26 +0530

    [vijay]Add:UC2-TestCase Of Maximum Float Number

--- FindMaxGenericTests/UnitTest1.cs
+++ FindMaxGenericTests/UnitTest1.cs
@@ -28,6 +28,26 @@ namespace FindMaxGenericTests
             Assert.AreEqual(32, result);
             //Assert.Pass();
         }
-        
+        [Test]
+        public void GivenMaxFloatNum_WhenAnalysed_ShouldReturnFirstMax()
+        {
+            double result = genericMethodForDouble.FindValue(26.22f, 20.33f, 22.55f);
+            Assert.AreEqual(26.22f, result);
+            //Assert.Pass();
+        }
+        [Test]
+        public void GivenMaxFloatNum_WhenAnalysed_ShouldReturnSecondMax()
+        {
+            double result = genericMethodForDouble.FindValue(16.22f, 20.33f, 12.55f);
+            Assert.AreEqual(20.33f, result);
+            //Assert.Pass();
+        }
+        [Test]
+        public void GivenMaxFloatNum_WhenAnalysed_ShouldReturnThirdMax()
+        {
+            double result = genericMethodForDouble.FindValue(16.22f, 20.33f, 22.55f);
+            Assert.AreEqual(22.55f, result);
+            //Assert.Pass();
+        }
     }
 }
